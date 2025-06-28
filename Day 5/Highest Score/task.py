51d9fa2a0091e8425ab5af51c5a3ca0b08ffcded
rabah babaci <4rabah@gmail.com>
2025-06-28 10:58:49 -0700

    implement day 5 tasks

--- Day 5/Highest Score/task.py
+++ Day 5/Highest Score/task.py
@@ -1,2 +1,30 @@
 student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
 print(range(1, 10))
+
+def my_sum(nums):
+    total = 0
+    for num in nums:
+        total += num
+    return total
+
+def my_min(nums):
+    min_num = nums[0]
+    for num in nums:
+        if num < min_num:
+            min_num = num
+    return min_num
+
+def my_max(nums):
+    max_num = nums[0]
+    for num in nums:
+        if num > max_num:
+            max_num = num
+    return max_num
+
+total_score = my_sum(student_scores)
+smallest_num = my_min(student_scores)
+largest_num = my_max(student_scores)
+
+print(total_score)
+print(f"Smallest score is: {smallest_num}")
+print(f"Largest score is: {largest_num}")
--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -1,3 +1,5 @@
+import random
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -7,4 +9,20 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+password = ""
+random_letters = ""
+randon_symbols = ""
+random_numbers = ""
 
+for l in range(nr_letters):
+    random_letters += random.choice(letters)
+for n in range(nr_numbers):
+    random_numbers += random.choice(numbers)
+for s in range(nr_symbols):
+    randon_symbols += random.choice(symbols)
+
+password += random_letters + random_numbers + randon_symbols
+
+shuffled_password = ''.join(random.sample(password, len(password)))
+
+print(f"Here is your password: {shuffled_password}")
