6471d48a6ceafc214f6e316a70eadb31e7124258
rabah babaci <4rabah@gmail.com>
2025-07-07 16:16:26 -0700

    work on debugging

--- Day 13/Describe the Problem/task.py
+++ Day 13/Describe the Problem/task.py
@@ -1,5 +1,5 @@
 def my_function():
-    for i in range(1, 20):
+    for i in range(1, 21):
         if i == 20:
             print("You got it")
 
@@ -8,5 +8,10 @@ my_function()
 
 # Describe the Problem - Write your answers as comments:
 # 1. What is the for loop doing?
+# The loop is iterating starting from 1 (included) up to 20 (excluded)
+
 # 2. When is the function meant to print "You got it"?
+# The if statement set the function to print "You got it" when i equals to 20
+
 # 3. What are your assumptions about the value of i?
+# I believe it will never reach 20, the max that it can get to is 19.
--- Day 13/Fix the Errors/task.py
+++ Day 13/Fix the Errors/task.py
@@ -1,3 +1,8 @@
-age = int(input("How old are you?"))
+try:
+    age = int(input("How old are you? "))
+except ValueError:
+    print("User typed an invalid number")
+    age = int(input("How old are you? "))
+
 if age > 18:
-print("You can drive at age {age}.")
+    print("You can drive at age {age}.")
